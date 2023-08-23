0d9fab3e56cafeca146b5f53a29290aaffb8e3b9
william.walkley <william.walkley@re-leased.com>
2023-08-24 11:46:56 +1200

    Add IngredientController.cs and Ingredient.cs

---
+++ FoodHub/Controllers/IngredientController.cs
@@ -0,0 +1,15 @@
+﻿using FoodHub.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodHub.Controllers;
+
+[ApiController]
+[Route( "api/ingredients" )]
+public sealed class IngredientController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<Ingredient> GetIngredients( )
+    {
+        return Ok( new Ingredient { Id = new Guid( ), Name = "Pasta" } );
+    }
+}
---
+++ FoodHub/Models/Ingredient.cs
@@ -0,0 +1,7 @@
+﻿namespace FoodHub.Models;
+
+public class Ingredient
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
